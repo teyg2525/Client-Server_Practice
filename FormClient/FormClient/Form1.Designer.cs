c0d1ae3ee7ea681b9a5a0e210567fefcb4154893
teyg2525 <30502234+teyg2525@users.noreply.github.com>
2019-01-30 11:22:20 +0200

    It's form client

---
+++ FormClient/FormClient/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FormClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(174, 221);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 292);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FormClient/FormClient/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Pipes;
+using System.Text;
+using System.Security.Principal;
+using System.Diagnostics;
+
+
+namespace FormClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Client client = new Client(Environment.GetCommandLineArgs()[0], richTextBox1);
+            client.Start();
+            Console.ReadKey();
+        }
+
+    }
+
+    public class Client
+    {
+        NamedPipeClientStream clientPipe;
+        int clientID;
+        RichTextBox tb;
+        public Client(string serverID,RichTextBox tb)
+        {
+            this.tb = tb;
+
+            clientID = Process.GetCurrentProcess().Id;
+            clientPipe = new NamedPipeClientStream(".", "pipe" + serverID,
+                PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.None);
+        }
+
+        public void Start()
+        {
+            clientPipe.Connect();
+            byte[] inBytes = new byte[100];
+            clientPipe.Read(inBytes, 0, 100);
+            string inStr = Encoding.ASCII.GetString(inBytes);
+            msg("Received from server: " + inStr);
+
+            //send
+            msg("Sending message to server...");
+            byte[] outBytes = new byte[100];
+            outBytes = Encoding.ASCII.GetBytes("Hello, server! I'm your client #" + clientID);
+            clientPipe.Write(outBytes, 0, outBytes.Length);
+            //чекаємо поки сервер отримає повідомлення
+            clientPipe.WaitForPipeDrain();
+            msg("The message has been received by server");
+        }
+
+        void msg(string str)
+        {
+            tb.Text += str + "\n";
+        }
+    }
+}
